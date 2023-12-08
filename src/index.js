9d94dfdfc98e2d7d775b19c7848b064b2a2ef8a9
Isha <ishadheke4@gmail.com>
2023-12-08 21:07:24 +0545

    changes made

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ root.render(
       <p className="text-4xl text-center text-secondary bg-primary font-heading ">Hello world</p>
       <p className="font-subheading text-white text-4xl">This is subheading</p>
       <p className="text-yellow-500 font-paragraph text-4xl">This is a simple text for movie recommendation system</p>
+      <p className="text-yellow-500 font-paragraph text-4xl">Let me check it</p>
+      
 
     </div>
 

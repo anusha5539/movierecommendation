9a7fe2ff2620c81948f84ba7bc4ffe617a1eac2e
Isha <ishadheke4@gmail.com>
2023-12-11 17:09:33 +0545

    sign added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SignIn from "./Components/Signin";
+import Signup from "./Components/Signup";
+function App() {
+  return (
+    < >
+    {/* <SignIn/> */}
+    <Signup/>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../src/index.css';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <App />
-  <>
-    <div className="h-screen bg-primary">
-      <p className="text-4xl text-center text-secondary bg-primary font-heading ">Hello world</p>
-      <p className="font-subheading text-white text-4xl">This is subheading</p>
-      <p className="text-yellow-500 font-paragraph text-4xl">This is a simple text for movie recommendation system</p>
-      <p className="text-yellow-500 font-paragraph text-4xl">Let me check it</p>
-      
+<App />
+ );
 
-    </div>
-
-  </>
-);
 
